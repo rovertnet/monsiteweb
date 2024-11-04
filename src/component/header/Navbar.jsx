e4f7249b4a35f89acc7f6aa0245b3c917e3ca335
Robert Matundu <matundukabamba@gmail.com>
2024-11-04 18:41:57 +0100

    first commit

--- src/component/header/Navbar.jsx
+++ src/component/header/Navbar.jsx
@@ -3,7 +3,9 @@
 export default function Navbar() {
   return (
     <>
-      
+      <div className="fixed top-0 left-0 right-0 bg-slate-500/5 px-5 py-2">
+        <div className="flex justify-between items-center"></div>
+      </div>
     </>
   )
 }
