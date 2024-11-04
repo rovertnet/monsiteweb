aec5926fea793cba0669be8b1d2a1368f6248d0b
Robert Matundu <matundukabamba@gmail.com>
2024-11-04 21:47:01 +0100

    first commit

--- src/component/body/About.jsx
+++ src/component/body/About.jsx
@@ -1,10 +1,23 @@
+import aboutimg from "../../assets/image/RP2.jpeg"
+
 
 export default function About() {
   return (
     <>
       <div className="block">
-        
+        <div className="flex flex-col-reverse md:flex-row gap-10 md:mx-36 mx-6">
+
+          <div className="">
+            
+          </div>
+
+          <img
+            src={aboutimg}
+            alt={aboutimg}
+            className="md:w-4/6 rounded-md border-[7px] border-slate-300 w-40 h-40 object-cover"
+          />
+        </div>
       </div>
     </>
-  )
+  );
 }
