a90a8cef3a88e4114388140636560a6bedb63b7e
Robert Matundu <matundukabamba@gmail.com>
2024-11-04 18:58:31 +0100

    first commit

--- src/component/header/Navbar.jsx
+++ src/component/header/Navbar.jsx
@@ -3,10 +3,10 @@ import logo from "../../assets/image/mon_logo.png"
 export default function Navbar() {
   return (
     <>
-      <div className="fixed top-0 left-0 right-0 bg-slate-500/5 px-5 py-2 md:px-5 md:py-2 mx-36 md:mx-36 my-5 md:my shadow-md rounded-xl">
+      <div className="fixed bottom-0 left-0 right-0 bg-slate-500/5 px-5 py-2 md:px-5 md:py-2 mx-36 md:mx-36 my-5 md:my shadow-md rounded-xl">
         <div className="flex justify-between items-center">
-          <img src={logo} alt={logo} className="w-12 h-12" />
-          
+          <img src={logo} alt={logo} className="w-10 h-10" />
+
         </div>
       </div>
     </>
