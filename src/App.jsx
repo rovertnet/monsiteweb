a07132a35915af243ae4a1f6952fd6b6916f47f8
Robert Matundu <matundukabamba@gmail.com>
2024-11-04 18:51:02 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Navbar from "./component/header/Navbar"
 
 
 function App() {
 
   return (
     <>
-      <div className="bg-slate-950"></div>
+      <div className="bg-slate-950 h-screen">
+        <Navbar />
+      </div>
     </>
   )
 }
