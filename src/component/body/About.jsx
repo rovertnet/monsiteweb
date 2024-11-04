c76039c7066aafd8496c6955c01c5cf7cfe7cbe5
Robert Matundu <matundukabamba@gmail.com>
2024-11-04 22:25:50 +0100

    first commit

--- src/component/body/About.jsx
+++ src/component/body/About.jsx
@@ -10,6 +10,7 @@ export default function About() {
             <h2 className="font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5">
               ROBERT ROVERTNET
             </h2>
+            <p className="font-medium text-lg text-slate-300"></p>
           </div>
 
           <img
