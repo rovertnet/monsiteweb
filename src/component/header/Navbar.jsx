bbf522f0d08044205b19b02004cfa3062b6378de
Robert Matundu <matundukabamba@gmail.com>
2024-11-04 19:00:39 +0100

    first commit

--- src/component/header/Navbar.jsx
+++ src/component/header/Navbar.jsx
@@ -1,4 +1,6 @@
-import logo from "../../assets/image/mon_logo.png"
+import logo from "../../assets/image/mon_logo.png";
+import { MdMenu } from "react-icons/md";
+
 
 export default function Navbar() {
   return (
@@ -6,9 +8,9 @@ export default function Navbar() {
       <div className="fixed bottom-0 left-0 right-0 bg-slate-500/5 px-5 py-2 md:px-5 md:py-2 mx-36 md:mx-36 my-5 md:my shadow-md rounded-xl">
         <div className="flex justify-between items-center">
           <img src={logo} alt={logo} className="w-10 h-10" />
-
+          <MdMenu size={12} />
         </div>
       </div>
     </>
-  )
+  );
 }
