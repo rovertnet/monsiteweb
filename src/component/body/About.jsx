0c3980889d21bc0078ffd2cefe50fe1c1f7c9530
Robert Matundu <matundukabamba@gmail.com>
2024-11-05 14:26:36 +0100

    first commit

--- src/component/body/About.jsx
+++ src/component/body/About.jsx
@@ -5,24 +5,27 @@ export default function About() {
   return (
     <>
       <div className="block h-screen pt-5 pb-10 bg-black">
-        <div className="flex flex-col-reverse md:flex-row gap-10 md:mx-36 mx-6">
+        <div className="flex flex-col-reverse md:flex-row gap-10 md:gap-40 md:mx-36 mx-6">
           <div className=" flex flex-col">
             <h2 className="font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5">
               ROBERT ROVERTNET
             </h2>
-            <p className="font-medium text-lg text-slate-300 py-3">
+            <p className="font-medium text-lg text-slate-300 py-3 text-justify md:max-w-xl">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
               eum rerum minus earum animi voluptatum deleniti laudantium ad
               officia alias? Magnam temporibus praesentium odio, laudantium
               alias cupiditate repudiandae reprehenderit cum!
             </p>
-            <div className="flex space-x-2 "></div>
+            <button className="font-semibold text-xl text-white bg-purple-400 hover:bg-purple-500 flex justify-between items-center">
+              CV 
+              
+            </button>
           </div>
 
           <img
             src={aboutimg}
-            alt={aboutimg}
-            className="md:w-4/6 md:h-80 w-full rounded-md border-[7px] border-slate-300  h-full object-cover"
+            alt={aboutimg} 
+            className="md:w-80 md:h-80 w-full rounded-md border-[7px] border-slate-300  h-80 object-cover"
           />
         </div>
       </div>
