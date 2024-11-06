33471967ed5499c9e2b8cf2d0673937f5a6e4dbc
Robert Matundu <matundukabamba@gmail.com>
2024-11-06 23:54:18 +0100

    another commit

--- src/component/body/Skils.jsx
+++ src/component/body/Skils.jsx
@@ -1,10 +1,4 @@
 
-import Timeline from "@mui/lab/Timeline";
-import TimelineItem from "@mui/lab/TimelineItem";
-import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector";
-import TimelineContent from "@mui/lab/TimelineContent";
-import TimelineDot from "@mui/lab/TimelineDot";
 
 export default function Skils() {
   return (
@@ -14,35 +8,7 @@ export default function Skils() {
           Education de ROBERT ROVERTNET
         </h2>
 
-        <Timeline position="alternate">
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineDot variant="outlined" />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>Eat</TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineDot variant="outlined" color="primary" />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>Code</TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineDot variant="outlined" color="secondary" />
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent>Sleep</TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineDot variant="outlined" />
-            </TimelineSeparator>
-            <TimelineContent>Repeat</TimelineContent>
-          </TimelineItem>
-        </Timeline>
+        <div className=""></div>
       </div>
     </>
   );
