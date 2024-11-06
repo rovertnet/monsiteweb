a0817bcf4c01c16c9a76da3fd509cbb64863bcb1
Robert Matundu <matundukabamba@gmail.com>
2024-11-07 00:20:14 +0100

    another commit

--- src/component/body/Skils.jsx
+++ src/component/body/Skils.jsx
@@ -1,17 +1,52 @@
 
 
-export default function Skils() {
-  return (
-    <>
-      <div className="block py-5">
-        <div className="flex justify-center items-center h-screen">
-          <h2 className="font-bold uppercase text-transparent text-center py-7 bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5">
-            Education de ROBERT ROVERTNET
-          </h2>
 
-          <div className=""></div>
-        </div>
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+
+export default function ColorTabs() {
+  const [value, setValue] = React.useState("one");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="block py-10 ">
+      <div className="flex justify-center items-center h-screen">
+        <Box sx={{ width: "100%" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            textColor="secondary"
+            indicatorColor="secondary"
+            aria-label="secondary tabs example"
+          >
+            <Tab value="one" label="Item One" />
+            <Tab value="two" label="Item Two" />
+            <Tab value="three" label="Item Three" />
+          </Tabs>
+        </Box>
       </div>
-    </>
+    </div>
   );
 }
+
+
+// export default function Skils() {
+//   return (
+//     <>
+//       <div className="block py-5">
+//         <div className="flex justify-center items-center h-screen">
+//           <h2 className="font-bold uppercase text-transparent text-center py-7 bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5">
+//             Education de ROBERT ROVERTNET
+//           </h2>
+
+//           <div className=""></div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
