177819ab4af31de50313f1b0c0d92de2d836df88
Robert Matundu <matundukabamba@gmail.com>
2024-11-06 23:57:37 +0100

    another commit

--- src/component/body/About.jsx
+++ src/component/body/About.jsx
@@ -5,29 +5,31 @@ import { FaDownload } from "react-icons/fa";
 export default function About() {
   return (
     <>
-      <div className="block h-screen pt-5 pb-10 bg-black">
-        <div className="flex flex-col-reverse md:flex-row gap-10 md:gap-40 md:mx-36 mx-6">
-          <div className=" flex flex-col">
-            <h2 className="font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5">
-              A propos de ROBERT ROVERTNET
-            </h2>
-            <p className="font-medium text-lg text-slate-300 py-3 text-justify md:max-w-xl">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-              eum rerum minus earum animi voluptatum deleniti laudantium ad
-              officia alias? Magnam temporibus praesentium odio, laudantium
-              alias cupiditate repudiandae reprehenderit cum!
-            </p>
-            <button className="font-semibold text-xl px-5 py-2 mt-6 mb-6 rounded-md text-white bg-purple-600 hover:bg-purple-500 flex justify-between items-center">
-              Téléchargez mon CV
-              <FaDownload />
-            </button>
-          </div>
+      <div className="block  pt-5 pb-10 bg-black">
+        <div className="flex justify-center items-center h-screen">
+          <div className="flex flex-col-reverse md:flex-row gap-10 md:gap-40 md:mx-36 mx-6">
+            <div className=" flex flex-col">
+              <h2 className="font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5">
+                A propos de ROBERT ROVERTNET
+              </h2>
+              <p className="font-medium text-lg text-slate-300 py-3 text-justify md:max-w-xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
+                eum rerum minus earum animi voluptatum deleniti laudantium ad
+                officia alias? Magnam temporibus praesentium odio, laudantium
+                alias cupiditate repudiandae reprehenderit cum!
+              </p>
+              <button className="font-semibold text-xl px-5 py-2 mt-6 mb-6 rounded-md text-white bg-purple-600 hover:bg-purple-500 flex justify-between items-center">
+                Téléchargez mon CV
+                <FaDownload />
+              </button>
+            </div>
 
-          <img
-            src={aboutimg}
-            alt={aboutimg}
-            className="md:w-80 md:h-80 w-full rounded-md border-[7px] border-slate-100  h-80 object-cover"
-          />
+            <img
+              src={aboutimg}
+              alt={aboutimg}
+              className="md:w-80 md:h-80 w-full rounded-md border-[7px] border-slate-100  h-80 object-cover"
+            />
+          </div>
         </div>
       </div>
     </>
