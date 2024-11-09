a1b750d5760399eb64f955055d1edfa1f3851cdc
Robert Matundu <matundukabamba@gmail.com>
2024-11-09 15:59:21 +0100

    another commit

--- src/component/body/About.jsx
+++ src/component/body/About.jsx
@@ -5,8 +5,8 @@ import { FaDownload } from "react-icons/fa";
 export default function About() {
   return (
     <>
-      <div className="block  pt-5 pb-10 bg-black">
-        <div className="flex justify-center items-center h-screen">
+      <div className="block bg-black">
+        <div className="flex justify-center items-center h-screen md:h-screen">
           <div className="flex flex-col-reverse md:flex-row gap-10 md:gap-40 md:mx-36 mx-6">
             <div className=" flex flex-col">
               <h2 className="font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5">
@@ -31,7 +31,7 @@ export default function About() {
                 Téléchargez mon CV
                 <FaDownload />
               </button>
-              
+
             </div>
 
             <img
