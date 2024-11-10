62de50374ad9b0c59df1a623317d9230dc02a7ad
Robert Matundu <matundukabamba@gmail.com>
2024-11-10 19:28:30 +0100

    another commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Skils from './component/body/Skils.jsx'
 import About from './component/body/About.jsx'
 import Navbar from './component/header/Navbar.jsx'
+import Project from './component/body/Project.jsx'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")).render(
           <Route path="/" exact={true} element={<App />} />
           <Route path="/skills" exact={true} element={<Skils />} />
           <Route path="/about" exact={true} element={<About />} />
+          <Route path="/projet" exact={true} element={<Project />} />
         </Routes>
       </BrowserRouter>
     </div>
