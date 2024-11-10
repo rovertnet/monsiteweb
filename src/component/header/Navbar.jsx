bb56911e0b02a38daa4fc677fa0c5afd05ed9fb3
Robert Matundu <matundukabamba@gmail.com>
2024-11-10 17:15:00 +0100

    another commit

--- src/component/header/Navbar.jsx
+++ src/component/header/Navbar.jsx
@@ -32,64 +32,64 @@ export default function Navbar() {
       </div>
 
       <div className="md:hidden">
-        <Menu
-          anchorEl={anchorEl}
-          id="account-menu"
-          open={open}
-          onClose={handleClose}
-          onClick={handleClose}
-          slotProps={{
-            paper: {
-              elevation: 0,
-              sx: {
-                overflow: "visible",
-                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                mt: 1.5,
-                "& .MuiAvatar-root": {
-                  width: 32,
-                  height: 32,
-                  ml: -0.5,
-                  mr: 1,
-                },
-                "&::before": {
-                  content: '""',
-                  display: "block",
-                  position: "absolute",
-                  top: 0,
-                  right: 14,
-                  width: 10,
-                  height: 10,
-                  bgcolor: "background.paper",
-                  zIndex: 0,
+        <div className="menu">
+          <Menu
+            anchorEl={anchorEl}
+            id="account-menu"
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            slotProps={{
+              paper: {
+                elevation: 0,
+                sx: {
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1.5,
+                  "& .MuiAvatar-root": {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  "&::before": {
+                    content: '""',
+                    display: "block",
+                    position: "absolute",
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: "background.paper",
+                    zIndex: 0,
+                  },
                 },
               },
-            },
-          }}
-          transformOrigin={{ horizontal: "right", vertical: "bottom" }}
-          anchorOrigin={{ horizontal: "right", vertical: "" }}
-          className="dropdown"
-        >
-          <MenuItem onClick={handleClose}>
-            <Avatar /> Profile
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <Avatar /> My account
-          </MenuItem>
-          <Divider />
-          <MenuItem onClick={handleClose}>
-            <ListItemIcon>
-              <PersonAdd fontSize="small" />
-            </ListItemIcon>
-            Add another account
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <ListItemIcon>
-              <Settings fontSize="small" />
-            </ListItemIcon>
-            Settings
-          </MenuItem>
-          <MenuItem onClick={handleClose}></MenuItem>
-        </Menu>
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "bottom" }}
+            anchorOrigin={{ horizontal: "right", vertical: "" }}
+            className="dropdown"
+          >
+            <MenuItem onClick={handleClose}>
+              <Avatar /> Accueil
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Avatar /> A propos
+            </MenuItem>
+
+            <MenuItem onClick={handleClose}>
+              <Avatar /> Services
+            </MenuItem>
+
+            <MenuItem onClick={handleClose}>
+              <Avatar /> Projets
+            </MenuItem>
+
+            <MenuItem onClick={handleClose}>
+              <Avatar /> Compétences
+            </MenuItem>
+          </Menu>
+        </div>
       </div>
     </>
   );
