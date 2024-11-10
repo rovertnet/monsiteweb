208943d0940448c9f6bdc5b9bcd2f3b759205e3f
Robert Matundu <matundukabamba@gmail.com>
2024-11-10 16:42:39 +0100

    another commit

--- src/component/header/Navbar.jsx
+++ src/component/header/Navbar.jsx
@@ -2,20 +2,11 @@
 import { useState } from "react";
 import logo from "../../assets/image/mon_logo.png";
 import { MdMenu } from "react-icons/md";
-import { NavLink } from "react-router-dom";
 
-const navigation = [
-  { name: "Home", to: "/" },
-  { name: "About", to: "/about" },
-  { name: "skills", to: "/skills" },
-  { name: "project", to: "/projet" },
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
 
 export default function Navbar() {
+
+  
   
   return (
     <>
