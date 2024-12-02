3e2ac246dfd83600edbe7b4f98dd8c80d9ce1f01
Robert Matundu <matundukabamba@gmail.com>
2024-12-02 19:18:23 +0100

    another commit

--- src/component/header/Navbar.jsx
+++ src/component/header/Navbar.jsx
@@ -15,9 +15,9 @@ export default function Navbar() {
     <>
       <div className="fixed bottom-0 md:bottom-0 left-0 right-0 bg-white/20 md:bg-white/20 border-[1px] border-slate-500 px-3 py-[3px] md:px-3 md:py-[3] mx-24 md:mx-[600px] my-3 md:my-10 shadow-md rounded-full md:rounded-3xl">
         <div className="flex justify-between items-center">
-          <img src={logo} alt={logo} className="w-9 md:w-10 md:h-10 h-9" />
+          <img src={logo} alt={logo} className="w-5 md:w-6 md:h-6 h-5" />
           <button
-            className="bg-slate-900 border-[1px] border-slate-300 px-2 py-2 rounded-full cursor-pointer"
+            className="bg-slate-900 px-0 py-0 rounded-full cursor-pointer"
             onClick={toggleMenu}
           >
             {openMenu ? (
