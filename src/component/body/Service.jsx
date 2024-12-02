18870381f45e9975c93a45ecb2e790b21227a779
Robert Matundu <matundukabamba@gmail.com>
2024-12-02 20:20:43 +0100

    another commit

--- src/component/body/Service.jsx
+++ src/component/body/Service.jsx
@@ -4,7 +4,11 @@ export default function Service() {
   return (
     <>
       <div className=" block pt-5 pb-10 bg-black">
-        
+        <div className="flex justify-center items-center gap-10">
+          <div className="flex space-x-5">
+            <img src="" className="" alt="" />
+          </div>
+        </div>
       </div>
     </>
   )
