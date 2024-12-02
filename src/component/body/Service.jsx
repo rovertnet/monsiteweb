49b93992ef46ee757091afe18e07f8b3b573ea07
Robert Matundu <matundukabamba@gmail.com>
2024-12-02 20:33:14 +0100

    another commit

--- src/component/body/Service.jsx
+++ src/component/body/Service.jsx
@@ -5,7 +5,12 @@ export default function Service() {
     <>
       <div className=" block pt-5 pb-10 bg-black">
         <div className="flex justify-center items-center gap-10">
-          <div className="flex space-x-5"></div>
+          <div className="flex space-x-5">
+            <SiGooglemarketingplatform className="text-3xl font-extrabold text-slate-300" />
+            <div className="flex flex-col gap-2">
+              <h3 className="Ma"></h3>
+            </div>
+          </div>
         </div>
       </div>
     </>
