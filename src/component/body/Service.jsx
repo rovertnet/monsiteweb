64b21a69a07c80d71003df96317742077bd47ee1
Robert Matundu <matundukabamba@gmail.com>
2024-12-02 22:21:34 +0100

    another commit

--- src/component/body/Service.jsx
+++ src/component/body/Service.jsx
@@ -4,9 +4,9 @@ export default function Service() {
   return (
     <>
       <div className=" block pt-10 pb-10 bg-black">
-        <h1 className="font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... uppercase text-center pb-10">
+        <h2 className="font-bold uppercase text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5">
           Mes Services
-        </h1>
+        </h2>
         <div className="md:mx-36 mx-6">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-6">
             <div className="py-5 px-5 rounded-lg border-[1px] border-slate-500 bg-black">
