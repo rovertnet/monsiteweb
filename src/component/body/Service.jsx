23baf00e52d246234ee7af0c0b28e97eadbb33d3
Robert Matundu <matundukabamba@gmail.com>
2024-12-02 20:28:47 +0100

    another commit

--- src/component/body/Service.jsx
+++ src/component/body/Service.jsx
@@ -1,15 +1,13 @@
-
+import { SiGooglemarketingplatform } from "react-icons/si";
 
 export default function Service() {
   return (
     <>
       <div className=" block pt-5 pb-10 bg-black">
         <div className="flex justify-center items-center gap-10">
-          <div className="flex space-x-5">
-            
-          </div>
+          <div className="flex space-x-5"></div>
         </div>
       </div>
     </>
-  )
+  );
 }
