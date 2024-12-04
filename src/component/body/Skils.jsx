43cd4ea92661d1dfe1272da1d402aef6c7b00901
Robert Matundu <matundukabamba@gmail.com>
2024-12-04 21:53:18 +0100

    another commit

--- src/component/body/Skils.jsx
+++ src/component/body/Skils.jsx
@@ -1,4 +1,4 @@
-
+import { FaHtml5 } from "react-icons/fa6";
 
 export default function Skils() {
   return (
@@ -10,12 +10,22 @@ export default function Skils() {
 
         <div className="md:mx-40 mx-6">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-5 py-10">
-            <div className="bg-white/5 px-5 py-5 rounded-md">
+            <div className="bg-white/5 px-5 py-5 rounded-lg">
               <h2 className="font-semibold text-xl text-purple-600">
                 Front-End
               </h2>
+
+              <div className="flex justify-between items-center py-5">
+                <p className="flex space-x-3">
+                  <FaHtml5 className="text-5xl font-extrabold text-blue-600 pt-1" />
+                  <span className="text-emerald-100 font-semibold text-lg"></span>
+                </p>
+                <span className="font-semibold text-lg text-emerald-100">
+                  99.9%
+                </span>
+              </div>
             </div>
-            <div className="bg-white/5 px-5 py-5 rounded-md">
+            <div className="bg-white/5 px-5 py-5 rounded-lg">
               <h2 className="font-semibold text-xl text-orange-600">
                 Back-End
               </h2>
