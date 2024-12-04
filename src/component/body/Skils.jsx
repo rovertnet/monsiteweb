729cdee5232bf3a3b3bc64be874b866351b9962f
Robert Matundu <matundukabamba@gmail.com>
2024-12-04 21:34:06 +0100

    another commit

--- src/component/body/Skils.jsx
+++ src/component/body/Skils.jsx
@@ -8,12 +8,12 @@ export default function Skils() {
           Education de ROBERT ROVERTNET
         </h2>
 
-        <div className="flex justify-center items-center flex-col md:flex gap-5 py-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 py-10">
           <div className="bg-white/5 px-5 py-5">
-            <h2 className="font-semibold text-xl text-purple-600">Front-end</h2>
+            <h2 className="font-semibold text-xl text-purple-600">Front-End</h2>
           </div>
           <div className="bg-white/5 px-5 py-5 ">
-            <h2 className="font-semibold text-xl text-orange-600">Front-end</h2>
+            <h2 className="font-semibold text-xl text-orange-600">Back-End</h2>
           </div>
         </div>
       </div>
