68b54a0dec323b8d7c046eea4877c57417bdba3a
Robert Matundu <matundukabamba@gmail.com>
2024-12-04 21:12:16 +0100

    another commit

--- src/component/body/Skils.jsx
+++ src/component/body/Skils.jsx
@@ -3,14 +3,12 @@
 export default function Skils() {
   return (
     <>
-      <div className="block py-5">
+      <div className="block pt-10 pb-10 bg-black">
         <h2 className="font-bold uppercase text-transparent text-center py-7 bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5">
           Education de ROBERT ROVERTNET
         </h2>
 
-        <div className="">
-          
-        </div>
+        <div className=""></div>
       </div>
     </>
   );
