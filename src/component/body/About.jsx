7565ed8ba350c8350f2473d02cb2b4ef3a77be96
Robert Matundu <matundukabamba@gmail.com>
2024-12-04 18:59:22 +0100

    another commit

--- src/component/body/About.jsx
+++ src/component/body/About.jsx
@@ -6,9 +6,10 @@ export default function About() {
   return (
     <>
       <div className="block bg-black pt-5 pb-10">
+        <div className="shape w-[40rem] h-[25rem] absolute"></div>
         <div className="flex justify-center items-center">
           <div className="flex flex-col-reverse md:flex-row gap-10 md:gap-40 md:mx-36 mx-6">
-            <div className="shape flex flex-col">
+            <div className="flex flex-col">
               <h2 className="font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5">
                 A propos de ROBERT ROVERTNET
               </h2>
