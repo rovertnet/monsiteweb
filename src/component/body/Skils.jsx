3612ca36f4a3c6230c0b852c0d863da23e090fab
Robert Matundu <matundukabamba@gmail.com>
2024-12-16 21:34:16 +0100

    another commit

--- src/component/body/Skils.jsx
+++ src/component/body/Skils.jsx
@@ -6,7 +6,7 @@ import { FaReact } from "react-icons/fa6";
 export default function Skils() {
   return (
     <>
-      <div className="block pt-10 pb-10 bg-black">
+      <div className="block pt-10 bg-black">
         <h2 className="font-bold uppercase text-transparent text-center py-7 bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5">
           Mes Compétences
         </h2>
