6b98e72bf157be8aad4889e3000fb2a72d690169
Robert Matundu <matundukabamba@gmail.com>
2024-12-16 21:46:52 +0100

    another commit

--- src/component/body/Project.jsx
+++ src/component/body/Project.jsx
@@ -9,8 +9,8 @@ export default function Project() {
           </h2>
 
           <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-            <div className="bg-white/5 rounded-tl-md rounded-tr-md">
-              <div className="bg-slate-700 py-10 px-5"></div>
+            <div className="bg-white rounded-tl-md rounded-tr-md">
+              <div className="bg-slate-700 rounded-md py-10 px-5"></div>
               <div className=""></div>
             </div>
           </div>
