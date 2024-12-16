d29809168ef45032ff47d1bed90489758224e217
Robert Matundu <matundukabamba@gmail.com>
2024-12-16 20:31:49 +0100

    another commit

--- src/component/body/Service.jsx
+++ src/component/body/Service.jsx
@@ -11,7 +11,7 @@ export default function Service() {
         <h2 className="font-bold uppercase text-center text-transparent bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-10">
           Mes Services
         </h2>
-        <div className="md:mx-36 mx-6">
+        <div className="md:mx-40 mx-6">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-6">
             <div className="py-5 px-5 rounded-lg border-[1px] border-slate-500 bg-white/5">
               <div className="flex space-x-5 ">
