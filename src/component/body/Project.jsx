7e0059d7466e187cd05f906205ca37648608aaef
Robert Matundu <matundukabamba@gmail.com>
2024-12-16 21:24:49 +0100

    another commit

--- src/component/body/Project.jsx
+++ src/component/body/Project.jsx
@@ -8,7 +8,7 @@ export default function Project() {
         </h2>
 
        <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-        <div className=""></div> 
+        <div className="bg-white/5 "></div> 
        </div>   
       </div>
     </>
