314ae361e9ff8844a71a4002633cbed59e9efd79
Robert Matundu <matundukabamba@gmail.com>
2024-12-21 17:47:44 +0100

    another commit

--- src/component/body/Temoins.jsx
+++ src/component/body/Temoins.jsx
@@ -3,7 +3,13 @@
 export default function Temoins() {
   return (
     <>
-      <div className=""></div>
+      <div className="block pt-10 bg-black">
+        <h2 className="font-bold uppercase text-transparent text-center py-7 bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5">
+          Temoignages
+        </h2>
+
+        
+      </div>
     </>
-  )
+  );
 }
