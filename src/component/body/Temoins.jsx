9d63a756393821c704dd81f4bade14637314efed
Robert Matundu <matundukabamba@gmail.com>
2024-12-21 17:43:57 +0100

    another commit

---
+++ src/component/body/Temoins.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Temoins() {
+  return (
+    <div>Temoins</div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,9 @@ import About from './component/body/About.jsx'
 import Navbar from './component/header/Navbar.jsx'
 import Project from './component/body/Project.jsx'
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="bg-black h-screen flex justify-center items-center">
