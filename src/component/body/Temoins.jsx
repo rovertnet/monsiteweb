127be3f422e72bb97defa57dcae48a9e635423e5
Robert Matundu <matundukabamba@gmail.com>
2024-12-22 18:55:42 +0100

    another commit

--- src/component/body/Temoins.jsx
+++ src/component/body/Temoins.jsx
@@ -84,15 +84,17 @@ export default function Temoins() {
             {
               temoignageData.map((item) => {
                 <div key={item.id}>
-                  <div className=" flex flex-col gap-4 p-8 shadow-lg rounded-md bg-white/10">
-                    <div className=" flex justify-between items-center gap-5" >
+                  <div className=" flex flex-col gap-4 shadow-lg py-5 px-5 rounded-lg border-[1px] border-slate-500 bg-white/5">
+                    <div className=" flex justify-between items-center gap-5">
                       <img
                         src={item.imgT}
                         alt={item.imgT}
                         className="w-16 h-16 rounded-full"
                       />
                       <div>
-                        <p className="font-semibold text-xl text-slate-100">{item.name}</p>
+                        <p className="font-semibold text-xl text-slate-100">
+                          {item.name}
+                        </p>
                         <p className="">{item.name}</p>
                       </div>
                     </div>
