c19391922615ffcb6a837ad9f7030d8d7be792fc
Robert Matundu <matundukabamba@gmail.com>
2024-12-29 19:22:22 +0100

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 
 import About from "./component/body/About";
 import Herosection from "./component/body/Herosection";
+import Progresse from "./component/body/Progresse";
 import Project from "./component/body/Project";
 import Service from "./component/body/Service";
 import Skils from "./component/body/Skils";
-import Temoins from "./component/body/Temoins";
 
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
   return (
     <>
       <div className="bg-black h-screen md:h-screen">
+        <Progresse />
         <Herosection />
         <About />
         <Service />
