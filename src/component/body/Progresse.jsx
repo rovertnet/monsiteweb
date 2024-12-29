4fba5f875fae4ebf817d894168c5b2e8c1f68313
Robert Matundu <matundukabamba@gmail.com>
2024-12-29 19:20:42 +0100

    another commit

--- src/component/body/Progresse.jsx
+++ src/component/body/Progresse.jsx
@@ -1,8 +1,13 @@
+import motion, { useScroll } from "framer-motion"
+
 
 export default function Progresse() {
+
+  const { scrollYProgress } = useScroll();
+
   return (
     <>
-      
+      <motion.div style={{ scaleX: scrollYProgress }}></motion.div>
     </>
-  )
+  );
 }
