2e5bad42f8ecd83db99ebdc76c7ce5b04da87bc0
Robert Matundu <matundukabamba@gmail.com>
2024-12-29 19:44:31 +0100

    another commit

--- src/component/body/Progresse.jsx
+++ src/component/body/Progresse.jsx
@@ -1,13 +1,25 @@
-import motion, { useScroll } from "framer-motion"
+import motion, { useAnimate } from "framer-motion"
+import { useEffect } from "react";
 
 
 export default function Progresse() {
 
-  const { scrollYProgress } = useScroll();
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    const controls = animate([
+      [scope.current, { x: "100%" }],
+      ["li", { opacity: 1 }],
+    ]);
+
+    controls.speed = 0.8;
+
+    return () => controls.stop();
+  }, []);
 
   return (
     <>
-      <motion.div style={{ scaleX: scrollYProgress }}></motion.div>
+      
     </>
   );
 }
