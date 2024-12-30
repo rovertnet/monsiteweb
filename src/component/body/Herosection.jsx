11aaa95a4229426530eafdf4b9f7c0e19f2674eb
Robert Matundu <matundukabamba@gmail.com>
2024-12-30 21:27:33 +0100

    another commit

--- src/component/body/Herosection.jsx
+++ src/component/body/Herosection.jsx
@@ -5,11 +5,12 @@ import { FaSquareFacebook } from "react-icons/fa6";
 import { FaTiktok } from "react-icons/fa6";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
+import {gsap} from "@gsap/react"
 
 
 export default function Herosection() {
 
-  const gsap.to(".box")
+  gsap.to(".box", {})
 
   return (
     <>
