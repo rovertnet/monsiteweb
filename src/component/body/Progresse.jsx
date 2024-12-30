5219b1c8ffa073d6064c8ee9c11a21964198a3f6
Robert Matundu <matundukabamba@gmail.com>
2024-12-30 12:11:19 +0100

    another commit

--- src/component/body/Progresse.jsx
+++ src/component/body/Progresse.jsx
@@ -2,11 +2,16 @@
 
 export default function Counter() {
 
-  
+  const count = useMotionValue(0);
+
+  useEffect(() => {
+    const controls = animate(count, 100, { duration: 5 });
+    return () => controls.stop();
+  }, []);
 
   return (
     <>
-
+      <>{count}</>
     </>
-  )
+  );
 }
