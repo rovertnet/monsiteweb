b5f1e27f712283de8c5935de782c570b3811faf4
Robert Matundu <matundukabamba@gmail.com>
2025-01-05 21:06:07 +0100

    another

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import About from "./component/body/About";
+import Contact from "./component/body/Contact";
 import Herosection from "./component/body/Herosection";
 import Progresse from "./component/body/Progresse";
 import Project from "./component/body/Project";
@@ -19,6 +20,7 @@ function App() {
         <Service />
         <Skils />
         <Project />
+        <Contact />
       </div>
     </>
   );
