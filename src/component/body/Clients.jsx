0bba9ccef38bf6624f4fbff62f86c389cf07e1f7
Robert Matundu <matundukabamba@gmail.com>
2025-01-05 22:47:28 +0100

    another

--- src/component/body/Clients.jsx
+++ src/component/body/Clients.jsx
@@ -6,7 +6,7 @@ export default function Clients() {
       <div className=" bg-black">
         <div className="mx-6 md:mx-36 pt-10 pb-10 bg-white/5 rounded-lg">
           <h2 className="font-bold uppercase text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-14">
-            Contact
+            Clients
           </h2>
         </div>
       </div>
