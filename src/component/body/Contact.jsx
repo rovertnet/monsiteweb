cfe212a50f2c90ac823d49caf92871b32d29c6d0
Robert Matundu <matundukabamba@gmail.com>
2025-01-05 21:02:52 +0100

    another

--- src/component/body/Contact.jsx
+++ src/component/body/Contact.jsx
@@ -2,6 +2,14 @@
 
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <>
+      <div className="block  bg-black">
+        <div className="mx-6 md:mx-36 pt-10 pb-14">
+          <h2 className="font-bold uppercase text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-14">
+            Contact
+          </h2>
+        </div>
+      </div>
+    </>
+  );
 }
