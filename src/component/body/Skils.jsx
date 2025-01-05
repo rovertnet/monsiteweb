f8f81e1a78a4fdfc48b00603aae54f5c86d69eca
Robert Matundu <matundukabamba@gmail.com>
2025-01-05 19:27:40 +0100

    another

--- src/component/body/Skils.jsx
+++ src/component/body/Skils.jsx
@@ -106,6 +106,18 @@ export default function Skils() {
 
                 <div className="flex justify-between items-center py-5">
                   <p className="flex space-x-3">
+                    <SiExpress className="text-5xl font-extrabold text-blue-200 pt-1" />
+                    <span className="text-emerald-100 font-semibold text-lg uppercase pt-3">
+                      Nest js
+                    </span>
+                  </p>
+                  <span className="font-semibold text-lg text-emerald-100">
+                    69.9%
+                  </span>
+                </div>
+
+                <div className="flex justify-between items-center py-5">
+                  <p className="flex space-x-3">
                     <GrMysql className="text-5xl font-extrabold text-blue-400 pt-1" />
                     <span className="text-emerald-100 font-semibold text-lg capitalize pt-3">
                       MySQL
