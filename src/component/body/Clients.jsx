91337fb10996cdc48343e17605c770d56d3747d3
Robert Matundu <matundukabamba@gmail.com>
2025-01-05 22:37:45 +0100

    another

--- src/component/body/Clients.jsx
+++ src/component/body/Clients.jsx
@@ -2,6 +2,14 @@
 
 export default function Clients() {
   return (
-    <div>Clients</div>
-  )
+    <>
+      <div className=" bg-black">
+        <div className="mx-6 md:mx-36 pt-10 pb-10 bg-white/5 rounded-lg">
+          <h2 className="font-bold uppercase text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-14">
+            Contact
+          </h2>
+        </div>
+      </div>
+    </>
+  );
 }
