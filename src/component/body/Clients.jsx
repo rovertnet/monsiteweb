a08ec6c758181d67181b1dbd871233f63f2a30b5
Robert Matundu <matundukabamba@gmail.com>
2025-01-11 13:20:48 +0100

    another commit

--- src/component/body/Clients.jsx
+++ src/component/body/Clients.jsx
@@ -8,6 +8,8 @@ export default function Clients() {
           <h2 className="font-bold uppercase text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-14">
             Clients
           </h2>
+
+          <div className=""></div>
         </div>
       </div>
     </>
