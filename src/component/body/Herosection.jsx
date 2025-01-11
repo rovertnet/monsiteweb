71d5c401a77709e9779afd9218202105fc316905
Robert Matundu <matundukabamba@gmail.com>
2025-01-11 23:28:58 +0100

    another commit

--- src/component/body/Herosection.jsx
+++ src/component/body/Herosection.jsx
@@ -27,13 +27,19 @@ export default function Herosection() {
     <>
       <div className="block">
         <div className=" h-screen md:h-screen lg:h-screen flex flex-col items-center justify-center ">
-          <h1 data-aos="zoom-in"  className="font-extrabold text-6xl md:text-8xl uppercase  text-center text-transparent bg-clip-text bg-gradient-to-b from-orange-400 from-10% via-white via-30% to-purple-700 to-90% ...">
+          <h1
+            data-aos="zoom-in"
+            className="font-extrabold text-6xl md:text-8xl uppercase  text-center text-transparent bg-clip-text bg-gradient-to-b from-orange-400 from-10% via-white via-30% to-purple-700 to-90% ..."
+          >
             Bonjour!
           </h1>
           <span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-xl md:text-text-3xl pt-3 pb-5 text-center">
             C'est Robert, Développeur Web!
           </span>
-          <div className="bg-white/10 rounded-full p-5 py-5 w-52 h-52 flex items-center justify-center">
+          <div
+            data-aos="zoom-in"
+            className=" bg-white/10 rounded-full p-5 py-5 w-52 h-52 flex items-center justify-center"
+          >
             <img
               src={rp}
               alt={rp}
