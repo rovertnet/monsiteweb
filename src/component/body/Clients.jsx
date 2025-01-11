9b7d2a5d1bc453d53ecc4093d2b1a8893b4e59ff
Robert Matundu <matundukabamba@gmail.com>
2025-01-11 20:10:34 +0100

    another commit

--- src/component/body/Clients.jsx
+++ src/component/body/Clients.jsx
@@ -16,7 +16,7 @@ export default function Clients() {
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.95 }}
               onHoverStart={() => console.log("hover started!")}
-              className="bg-white/5 px-7 py-7 hover:border-[1px] hover:border-slate-400"
+              className="bg-white/5 px-7 py-7 hover:border-[1px] hover:border-slate-400 rounded-md cursor-pointer"
             >
               <FaQuoteLeft className="text-purple-500 text-5xl text-center font-bold pb-3" />
               <p className="text-justify text-slate-100 font-medium pb-10">
@@ -42,7 +42,7 @@ export default function Clients() {
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.95 }}
               onHoverStart={() => console.log("hover started!")}
-              className="bg-white/5 px-7 py-7"
+              className="bg-white/5 px-7 py-7 hover:border-[1px] hover:border-slate-400 rounded-md cursor-pointer"
             >
               <FaQuoteLeft className="text-purple-500 text-5xl text-center font-bold pb-3" />
               <p className="text-justify text-slate-100 font-medium pb-10">
@@ -68,7 +68,7 @@ export default function Clients() {
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.95 }}
               onHoverStart={() => console.log("hover started!")}
-              className="bg-white/5 px-7 py-7"
+              className="bg-white/5 px-7 py-7 hover:border-[1px] hover:border-slate-400 rounded-md cursor-pointer"
             >
               <FaQuoteLeft className="text-purple-500 text-5xl text-center font-bold pb-3" />
               <p className="text-justify text-slate-100 font-medium pb-10">
