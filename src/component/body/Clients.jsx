f2348199a64f831ebd044f26ec0b1d8ebfbc1136
Robert Matundu <matundukabamba@gmail.com>
2025-01-11 13:29:31 +0100

    another commit

--- src/component/body/Clients.jsx
+++ src/component/body/Clients.jsx
@@ -3,14 +3,14 @@
 export default function Clients() {
   return (
     <>
-      <div className=" bg-black pt-10 pb-10">
-        <div className="mx-6 md:mx-36 pt-10 pb-10  rounded-lg">
+      <div className=" bg-black pt-10 pb-10 block">
           <h2 className="font-bold uppercase text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-14">
             Clients
           </h2>
 
-          <div className="bg-white/5  md:mx-40 mx-6"></div>
-        </div>
+          <div className="bg-white/5  md:mx-40 mx-6">
+            
+          </div>
       </div>
     </>
   );
