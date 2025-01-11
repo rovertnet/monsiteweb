bf4d93d4ffdcade85337c6763da6abb1afd669e4
Robert Matundu <matundukabamba@gmail.com>
2025-01-11 14:31:13 +0100

    another commit

--- src/component/body/Clients.jsx
+++ src/component/body/Clients.jsx
@@ -1,20 +1,20 @@
-
+import { FaQuoteLeft } from "react-icons/fa";
 
 export default function Clients() {
   return (
     <>
       <div className=" bg-black pt-10 pb-10 block">
-          <h2 className="font-bold uppercase text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-14">
-            Clients
-          </h2>
+        <h2 className="font-bold uppercase text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-14">
+          Clients
+        </h2>
 
-          <div className="bg-white/5  md:mx-40 mx-6">
-            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 sm:grid-cols-3 gap-10">
-              <div className="bg-white/5 px-7 py-7">
-                
-              </div>
+        <div className=" md:mx-40 mx-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 sm:grid-cols-3 gap-10">
+            <div className="bg-white/5 px-7 py-7">
+              <FaQuoteLeft className="" />
             </div>
           </div>
+        </div>
       </div>
     </>
   );
