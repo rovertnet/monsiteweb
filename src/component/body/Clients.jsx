7efc9fcaea9084bbdc22c3b70ba27c6f122c6b98
Robert Matundu <matundukabamba@gmail.com>
2025-01-12 13:54:07 +0100

    another commit

--- src/component/body/Clients.jsx
+++ src/component/body/Clients.jsx
@@ -6,7 +6,13 @@ export default function Clients() {
   return (
     <>
       <div className=" bg-black pt-10 pb-10 block">
-        <h2 className="font-bold uppercase text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-14">
+        <h2
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+          data-aos-delay="100"
+          className="font-bold uppercase text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-14"
+        >
           Clients
         </h2>
 
