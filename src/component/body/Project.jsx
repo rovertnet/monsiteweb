58af6163736959857118dff40fa1cdd75c00b352
Robert Matundu <matundukabamba@gmail.com>
2025-01-12 13:47:01 +0100

    another commit

--- src/component/body/Project.jsx
+++ src/component/body/Project.jsx
@@ -24,12 +24,23 @@ export default function Project() {
     <>
       <div className="block  bg-black">
         <div className="mx-6 md:mx-36 pt-10 pb-14">
-          <h2 className="font-bold uppercase text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-14">
+          <h2
+            data-aos="fade-down"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+            data-aos-delay="100"
+            className="font-bold uppercase text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-14"
+          >
             Mes Projets
           </h2>
 
           <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-            <div className="bg-white/5 rounded-md">
+            <div
+              data-aos="fade-up"
+              data-aos-duration="1500"
+              data-aos-delay="200"
+              className="bg-white/5 rounded-md"
+            >
               <div className="">
                 <img
                   src={proimg2}
@@ -58,7 +69,12 @@ export default function Project() {
               </div>
             </div>
 
-            <div className="bg-white/5 rounded-md">
+            <div
+              data-aos="fade-up"
+              data-aos-duration="1500"
+              data-aos-delay="400"
+              className="bg-white/5 rounded-md"
+            >
               <div className="">
                 <img
                   src={proimg3}
@@ -87,7 +103,12 @@ export default function Project() {
               </div>
             </div>
 
-            <div className="bg-white/5 rounded-md">
+            <div
+              data-aos="fade-up"
+              data-aos-duration="1500"
+              data-aos-delay="600"
+              className="bg-white/5 rounded-md"
+            >
               <div className="">
                 <img
                   src={proimg1}
