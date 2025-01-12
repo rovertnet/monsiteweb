53d61ccee09bbe6c71c35590232faaa8dd21e16d
Robert Matundu <matundukabamba@gmail.com>
2025-01-12 13:50:40 +0100

    another commit

--- src/component/body/Skils.jsx
+++ src/component/body/Skils.jsx
@@ -16,13 +16,24 @@ export default function Skils() {
   return (
     <>
       <div className="block pt-10 bg-black">
-        <h2 className="font-bold uppercase text-transparent text-center py-7 bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5">
+        <h2
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+          data-aos-delay="100"
+          className="font-bold uppercase text-transparent text-center py-7 bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-5"
+        >
           Mes Compétences
         </h2>
 
         <div className="md:mx-40 mx-6">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-5 py-10">
-            <div className="bg-white/5 px-7 py-5 rounded-lg">
+            <div
+              data-aos="fade-up"
+              data-aos-duration="1500"
+              data-aos-delay="300"
+              className="bg-white/5 px-7 py-5 rounded-lg"
+            >
               <h2 className="font-semibold text-xl text-center text-purple-600">
                 Front-End
               </h2>
@@ -103,7 +114,12 @@ export default function Skils() {
             </div>
 
             {/* la parie backend */}
-            <div className="bg-white/5 px-7 py-5 rounded-lg">
+            <div
+              data-aos="fade-up"
+              data-aos-duration="1500"
+              data-aos-delay="600"
+              className="bg-white/5 px-7 py-5 rounded-lg"
+            >
               <h2 className="font-semibold text-xl text-center text-orange-600">
                 Back-End
               </h2>
