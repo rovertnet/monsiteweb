eefccfb0a4f11419dfce7c4442e70a9b3ca26ae4
Robert Matundu <matundukabamba@gmail.com>
2025-01-12 17:56:16 +0100

    another commit

--- src/component/body/Contact.jsx
+++ src/component/body/Contact.jsx
@@ -16,7 +16,7 @@ export default function Contact() {
 
         <div className="md:mx-40 mx-6">
           <div className="flex flex-col md:flex gap-6">
-            
+            <div className=""></div>
           </div>
         </div>
       </div>
