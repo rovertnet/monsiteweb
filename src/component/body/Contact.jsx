e97f2cbfb1655fb98c519b5204314b74957d558f
Robert Matundu <matundukabamba@gmail.com>
2025-01-12 21:04:59 +0100

    another commit

--- src/component/body/Contact.jsx
+++ src/component/body/Contact.jsx
@@ -23,32 +23,32 @@ export default function Contact() {
           <div className="flex gap-6">
             <div className="bg-white/5 rounded-md px-7 py-7">
               <div className="flex space-x-2 py-5">
-                <FaPhoneAlt className="text-xl font-bold text-slate-100" />
-                <span className="text-2xl font-bold text-slate-300">
+                <FaPhoneAlt className="text-2xl font-bold text-slate-100" />
+                <span className="text-lg font-bold text-slate-300">
                   +243 998724266
                 </span>
               </div>
               <div className="flex space-x-2 pb-5">
                 <IoMail className="text-2xl font-bold text-slate-100" />
-                <span className="text-xl font-bold text-slate-300">
+                <span className="text-lg font-bold text-slate-300">
                   matundukabamba@gmail.com
                 </span>
               </div>
               <div className="flex space-x-2 pb-5">
                 <BsWhatsapp className="text-2xl font-bold text-green-500" />
-                <span className="text-xl font-bold text-slate-300">
+                <span className="text-lg font-bold text-slate-300">
                   +243 823444705
                 </span>
               </div>
               <div className="flex space-x-2">
                 <FaLinkedin className="text-2xl font-bold text-blue-900" />
-                <span className="text-xl font-bold text-slate-300">
+                <span className="text-lg font-bold text-slate-300">
                   Robert Matundu
                 </span>
               </div>
             </div>
 
-            <div className="">
+            <div className=" w-full">
               <form action="">
                 <div className="flex flex-col md:flex-col gap-5">
                   <input
