7b751759ea05c2a098cb5d6ef4840e697d0ba508
Robert Matundu <matundukabamba@gmail.com>
2025-01-12 13:41:55 +0100

    another commit

--- src/component/body/Service.jsx
+++ src/component/body/Service.jsx
@@ -38,7 +38,7 @@ export default function Service() {
               onHoverStart={() => console.log("hover started!")}
               className="py-5 px-5 rounded-lg border-[1px] border-slate-500 bg-white/5 cursor-pointer"
               data-aos="fade-up"
-              data-aos-duration="3000"
+              data-aos-duration="1500"
               data-aos-delay="200"
             >
               <div className="flex space-x-5 ">
@@ -66,7 +66,7 @@ export default function Service() {
               onHoverStart={() => console.log("hover started!")}
               className="py-5 px-5 rounded-lg border-[1px] border-slate-500 bg-white/5 cursor-pointer"
               data-aos="fade-up"
-              data-aos-duration="3000"
+              data-aos-duration="1500"
               data-aos-delay="400"
             >
               <div className="flex space-x-5 ">
@@ -94,7 +94,7 @@ export default function Service() {
               onHoverStart={() => console.log("hover started!")}
               className="py-5 px-5 rounded-lg border-[1px] border-slate-500 bg-white/5 cursor-pointer"
               data-aos="fade-up"
-              data-aos-duration="3000"
+              data-aos-duration="1500"
               data-aos-delay="600"
             >
               <div className="flex space-x-5 ">
