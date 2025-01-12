f62f981d90c82928ae0e4ca078ef4ce4a857da6e
Robert Matundu <matundukabamba@gmail.com>
2025-01-12 17:35:30 +0100

    another commit

--- src/component/body/Contact.jsx
+++ src/component/body/Contact.jsx
@@ -4,11 +4,15 @@ export default function Contact() {
   return (
     <>
       <div className=" bg-black">
-          <h2 className="font-bold uppercase text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-14">
-            Contact
-          </h2>
-
-          
+        <h2
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+          data-aos-delay="100"
+          className="font-bold uppercase text-transparent text-center bg-clip-text bg-gradient-to-r from-purple-700 from-10% via-white via-30% to-orange-500 to-90% ... text-2xl md:text-text-3xl pt-3 pb-14"
+        >
+          Contact
+        </h2>
       </div>
     </>
   );
