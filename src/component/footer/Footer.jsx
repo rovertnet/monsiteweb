82005d1c003e07e0efa517539d4482dd61eb0666
Robert Matundu <matundukabamba@gmail.com>
2025-01-13 22:58:25 +0100

    another commit

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <>
-      <div className="bg-white/5 p-10">
+      <div className="bg-white/5 pt-12">
         <p className=" text-gray-300 text-lg text-center md:text-sm">
           © RovertNet {new Date().getFullYear()}. tous droits réservés
         </p>
