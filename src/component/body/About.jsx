29f9c34cc5dcfa5a8c63aedf3d9e3eaa28f17672
Robert Matundu <matundukabamba@gmail.com>
2025-01-22 15:51:41 +0100

    last commit

--- src/component/body/About.jsx
+++ src/component/body/About.jsx
@@ -77,7 +77,7 @@ export default function About() {
               data-aos-easing="linear"
               data-aos-duration="1500"
               data-aos-delay="400"
-              className="md:w-96 md:h-96 w-96 rounded-md border-[10px] border-slate-100  h-96 object-cover"
+              className="md:w-96 md:h-96 lg:w-96 lg:h-96 w-96 rounded-md border-[10px] border-slate-100  h-96 object-cover"
             />
           </div>
         </div>
